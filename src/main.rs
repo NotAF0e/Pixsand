0f604d3b012d37aa55264cf7eda57726167e467a
Phonixed456 <pawel.zdunowski123@gmail.com>
2023-04-20 17:46:31 +0100

    Added pixel perfect scaling, fps counter and more

--- src/main.rs
+++ src/main.rs
@@ -1,155 +1,3 @@
-// struct TileWorld {
-//     tiles: Vec<Vec<Tile>>,
-//     width: usize,
-//     height: usize,
-// }
-
-// #[derive(Clone, Debug)]
-// struct Tile {
-//     filled: bool,
-// }
-
-// impl Iterator for Tile {
-//     type Item = Tile;
-//     fn next(&mut self) -> Option<Self::Item> {
-//         None
-//     }
-// }
-
-// impl Tile {
-//     fn new(filled: bool) -> Self {
-//         Self { filled }
-//     }
-// }
-
-// impl TileWorld {
-//     fn create(&mut self) -> &mut Self {
-//         let mut rng = rand::thread_rng();
-
-//         self.tiles = vec![vec![]; self.height];
-//         for x in 0..self.height {
-//             for _y in 0..self.width {
-//                 self.tiles[x].push(Tile::new(rng.gen_bool(random())));
-//             }
-//         }
-
-//         for row in &self.tiles {
-//             for tile in row {}
-//         }
-//         return self;
-//     }
-
-//     fn print(&mut self) {
-//         let mut tiles_to_print = "".to_string();
-
-//         for row in &self.tiles {
-//             for tile in row {
-//                 if tile.filled {
-//                     tiles_to_print += "█";
-//                 } else {
-//                     tiles_to_print += " ";
-//                 }
-//             }
-//             tiles_to_print += "\n";
-//         }
-//         println!("{}", tiles_to_print);
-//     }
-// }
-
-// fn main() {
-//     let mut world = TileWorld {
-//         tiles: vec![vec![Tile { filled: false }]],
-//         width: 100,
-//         height: 100,
-//     };
-
-//     let mut world = world.create();
-//     world.print();
-// }
-
-// use macroquad::prelude::*;
-
-// #[derive(PartialEq, Clone)]
-// struct TileWorld {
-//     tiles: Vec<Vec<Tile>>,
-//     width: usize,
-//     height: usize,
-// }
-
-// #[derive(Clone, Debug, PartialEq)]
-// struct Tile {
-//     filled: bool,
-// }
-
-// impl Iterator for Tile {
-//     type Item = Tile;
-//     fn next(&mut self) -> Option<Self::Item> {
-//         None
-//     }
-// }
-
-// impl Tile {
-//     fn new(filled: bool) -> Self {
-//         Self { filled }
-//     }
-// }
-
-// impl TileWorld {
-//     fn create(&mut self) -> &mut Self {
-//         self.tiles = Vec::with_capacity(self.width);
-//         for _ in 0..self.width {
-//             let mut row = Vec::with_capacity(self.height);
-//             for _ in 0..self.height {
-//                 row.push(Tile::new(true));
-//             }
-//             self.tiles.push(row);
-//         }
-//         self
-//     }
-// }
-
-// #[macroquad::main("Pixsand")]
-// async fn main() {
-//     let pix_size: f32 = 5.0;
-
-//     let mut world = TileWorld {
-//         tiles: vec![vec![Tile { filled: false }]],
-//         width: 1920,
-//         height: 1080,
-//     };
-//     let mut world = world.create();
-
-//     loop {
-//         clear_background(BLACK);
-
-//         for (x_pos, row) in world.tiles.iter().enumerate() {
-//             for (y_pos, tile) in row.iter().enumerate() {
-//                 if tile.filled {
-//                     draw_rectangle(
-//                         x_pos as f32,
-//                         y_pos as f32,
-//                         pix_size,
-//                         pix_size,
-//                         Color::new(0.99, 0.98, 0.0, 1.0),
-//                     );
-//                 }
-//             }
-//         }
-//         // Optimised version of the above is:
-
-//         let (mouse_x, mouse_y) = mouse_position();
-//         draw_rectangle(
-//             mouse_x,
-//             mouse_y,
-//             pix_size,
-//             pix_size,
-//             Color::new(1.0, 0.0, 0.0, 1.0),
-//         );
-
-//         next_frame().await
-//     }
-// }
-
 use macroquad::prelude::*;
 
 #[derive(PartialEq, Clone)]
@@ -187,42 +35,77 @@ impl TileWorld {
     }
 }
 
-#[macroquad::main("Pixsand")]
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Pixsand".to_owned(),
+        fullscreen: true,
+        ..Default::default()
+    }
+}
+#[macroquad::main(window_conf)]
 async fn main() {
-    let pix_size: f32 = 5.0;
+    const PIX_SIZE: f32 = 20.0;
+    let screen_width: usize = screen_width() as usize;
+    let screen_height: usize = screen_height() as usize;
+
     let mut world = TileWorld {
         tiles: vec![vec![Tile { filled: false }]],
-        width: 1920,
-        height: 1080,
+        width: screen_width / PIX_SIZE as usize,
+        height: screen_height / PIX_SIZE as usize,
     };
-    let mut world = world.create();
+
+    let world = world.create();
 
     let mut image = Image::gen_image_color(
-        world.width as u16,
-        world.height as u16,
+        screen_width as u16,
+        screen_height as u16,
         Color::new(0.0, 0.0, 0.0, 1.0),
     );
     let texture = Texture2D::from_image(&image);
 
+    let mut last_world: TileWorld = world.clone();
+    last_world.set_tile(0, 0, true);
+
     loop {
         clear_background(BLACK);
         let (mouse_x, mouse_y) = mouse_position();
+        let mut change = false;
+        let (downscalled_mouse_x, downscalled_mouse_y) = (mouse_x / PIX_SIZE, mouse_y / PIX_SIZE);
 
-        for (x_pos, row) in world.tiles.iter().enumerate() {
-            for (y_pos, tile) in row.iter().enumerate() {
-                if tile.filled {
-                    let x = x_pos as f32;
-                    let y = y_pos as f32;
-                    let color = Color::new(0.99, 0.98, 0.0, 1.0);
-                    if image.get_pixel(x as u32, y as u32) != color {
-                        // Creates a square of pixels
-                        for x_square in 0..pix_size as u32 {
-                            for y_square in 0..pix_size as u32 {
-                                image.set_pixel(
-                                    (x + x_square as f32 - pix_size / 2.0) as u32,
-                                    (y + y_square as f32 - pix_size / 2.0) as u32,
-                                    color,
-                                );
+        // Handle mouse events
+        if is_mouse_button_down(MouseButton::Left) {
+            world.set_tile(
+                downscalled_mouse_x as usize,
+                downscalled_mouse_y as usize,
+                true,
+            );
+            change = true;
+        }
+        if is_mouse_button_down(MouseButton::Right) {
+            world.set_tile(
+                downscalled_mouse_x as usize,
+                downscalled_mouse_y as usize,
+                false,
+            );
+            change = true;
+        }
+
+        // Construct tile image
+        if &mut last_world != world || change {
+            for (x_pos, row) in world.tiles.iter().enumerate() {
+                for (y_pos, tile) in row.iter().enumerate() {
+                    let x = x_pos as f32 * PIX_SIZE;
+                    let y = y_pos as f32 * PIX_SIZE;
+
+                    let color: Color = if tile.filled {
+                        Color::new(0.99, 0.98, 0.0, 1.0)
+                    } else {
+                        Color::new(0.0, 0.0, 0.0, 1.0)
+                    };
+                    for x_pix in 0..(PIX_SIZE) as u32 {
+                        for y_pix in 0..(PIX_SIZE) as u32 {
+                            if image.get_pixel(x as u32 + x_pix, y as u32 + y_pix) != color {
+                                image.set_pixel(x as u32 + x_pix, y as u32 + y_pix, color);
                             }
                         }
                     }
@@ -230,24 +113,26 @@ async fn main() {
             }
         }
 
-        if is_mouse_button_down(MouseButton::Left) {
-            world.set_tile(mouse_x as usize, mouse_y as usize, true);
-        }
-        if is_mouse_button_down(MouseButton::Right) {
-            world.set_tile(mouse_x as usize, mouse_y as usize, false);
-        }
-
+        // Draw tile image
         texture.update(&image);
-
         draw_texture(texture, 0.0, 0.0, WHITE);
 
+        // Draw texture
         draw_rectangle(
-            mouse_x - pix_size / 2.0,
-            mouse_y - pix_size / 2.0,
-            pix_size,
-            pix_size,
+            mouse_x - PIX_SIZE / 2.0,
+            mouse_y - PIX_SIZE / 2.0,
+            PIX_SIZE,
+            PIX_SIZE,
             Color::new(1.0, 0.0, 0.0, 1.0),
         );
+
+        // Draw fps text
+        let fps: &str = &get_fps().to_string();
+        draw_text(fps, 10.0, 15.0, 25.0, WHITE);
+
+        if &mut last_world != world || change {
+            last_world = world.clone();
+        }
 
         next_frame().await
     }
