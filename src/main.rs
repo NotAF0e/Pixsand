6fc095e59952d169a7f71a56980fcb1b0a6e4c29
Phonixed456 <pawel.zdunowski123@gmail.com>
2023-04-21 17:13:13 +0100

    Improved code and added debug info

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use macroquad::prelude::*;
+use rayon::prelude::*;
+
+use std::time::*;
 
 #[derive(Clone, PartialEq)]
 struct TileWorld {
@@ -9,33 +12,34 @@ struct TileWorld {
 
 #[derive(Clone, Debug, PartialEq)]
 struct Tile {
-    filled: TileName,
+    filled: TileType,
     color: Color,
 }
 
 impl Tile {
-    fn new(filled: TileName) -> Self {
+    fn new(filled: TileType) -> Self {
         // Match the color to the correct color
         let color = match filled {
-            TileName::Air => Color::new(0.0, 0.0, 0.0, 1.0),
-            TileName::Sand => Color::new(0.99, 0.98, 0.0, 1.0),
-            TileName::Stone => Color::new(0.502, 0.502, 0.502, 1.0),
+            TileType::Air => Color::new(0.0, 0.0, 0.0, 1.0),
+            TileType::Sand => Color::new(0.99, 0.98, 0.0, 1.0),
+            TileType::Stone => Color::new(0.502, 0.502, 0.502, 1.0),
         };
         Self { filled, color }
     }
     fn match_color(&self) -> Color {
         match self.filled {
-            TileName::Air => Color::new(0.0, 0.0, 0.0, 1.0),
-            TileName::Sand => Color::new(0.99, 0.98, 0.0, 1.0),
-            TileName::Stone => Color::new(0.502, 0.502, 0.502, 1.0),
+            TileType::Air => Color::new(0.0, 0.0, 0.0, 1.0),
+            TileType::Sand => Color::new(0.99, 0.98, 0.0, 1.0),
+            TileType::Stone => Color::new(0.502, 0.502, 0.502, 1.0),
         }
     }
-    fn fall(&mut self, tiles: &mut Vec<Vec<Tile>>, x: usize, y: usize) {
+
+    fn fall(&mut self, tiles: &mut Vec<Vec<Tile>>, tile_type: TileType, x: usize, y: usize) {
         if let Some(row) = tiles.get_mut(x) {
             if let Some(tile_below) = row.get_mut(y + 1) {
-                if self.filled != TileName::Air && tile_below.filled == TileName::Air {
-                    tile_below.filled = TileName::Sand;
-                    tiles[x][y].filled = TileName::Air;
+                if self.filled != TileType::Air && tile_below.filled == TileType::Air {
+                    tile_below.filled = tile_type;
+                    tiles[x][y].filled = TileType::Air;
                 }
             }
         }
@@ -43,7 +47,7 @@ impl Tile {
 }
 
 #[derive(Clone, Debug, PartialEq)]
-enum TileName {
+enum TileType {
     Air,
     Sand,
     Stone,
@@ -55,7 +59,7 @@ impl TileWorld {
         for _ in 0..self.width {
             let mut row = Vec::with_capacity(self.height);
             for _ in 0..self.height {
-                row.push(Tile::new(TileName::Air));
+                row.push(Tile::new(TileType::Air));
             }
             self.tiles.push(row);
         }
@@ -64,7 +68,9 @@ impl TileWorld {
     fn update_sim(&mut self) {
         for (x_pos, row) in self.tiles.clone().iter_mut().enumerate() {
             for (y_pos, tile) in row.iter_mut().enumerate() {
-                tile.fall(&mut self.tiles, x_pos, y_pos);
+                if tile.filled == TileType::Sand {
+                    tile.fall(&mut self.tiles, tile.filled.clone(), x_pos, y_pos);
+                }
             }
         }
     }
@@ -79,13 +85,15 @@ fn window_conf() -> Conf {
 }
 #[macroquad::main(window_conf)]
 async fn main() {
-    const TILE_SIZE: f32 = 20.0;
+    const TILE_SIZE: f32 = 5.0;
     let screen_width: usize = screen_width() as usize;
     let screen_height: usize = screen_height() as usize;
 
+    let mut tile_type_selected: Option<TileType> = Some(TileType::Sand);
+
     let mut world = TileWorld {
         tiles: vec![vec![Tile {
-            filled: TileName::Air,
+            filled: TileType::Air,
             color: Color::new(0.0, 0.0, 0.0, 1.0),
         }]],
         width: screen_width / TILE_SIZE as usize,
@@ -101,24 +109,34 @@ async fn main() {
     );
     let texture = Texture2D::from_image(&image);
 
-
     loop {
         clear_background(BLACK);
         let (mouse_x, mouse_y) = mouse_position();
         let (downscalled_mouse_x, downscalled_mouse_y) = (mouse_x / TILE_SIZE, mouse_y / TILE_SIZE);
+        let fps: &str = &get_fps().to_string();
 
+        let update_sim_time = Instant::now();
         world.update_sim();
+        let update_sim_time = &update_sim_time.elapsed().as_secs_f32().to_string();
 
-        // Handle mouse events
+        // Handle input events
         if is_mouse_button_down(MouseButton::Left) {
             world.tiles[downscalled_mouse_x as usize][downscalled_mouse_y as usize].filled =
-                TileName::Sand;
+                tile_type_selected.clone().unwrap();
         }
         if is_mouse_button_down(MouseButton::Right) {
             world.tiles[downscalled_mouse_x as usize][downscalled_mouse_y as usize].filled =
-                TileName::Air;
+                TileType::Air;
+        }
+        if is_key_pressed(KeyCode::E) {
+            tile_type_selected = match tile_type_selected.unwrap() {
+                TileType::Air => None,
+                TileType::Sand => Some(TileType::Stone),
+                TileType::Stone => Some(TileType::Sand),
+            };
         }
 
+        let tile_image_time = Instant::now();
         // Construct tile image
         for (x_pos, row) in world.tiles.iter().enumerate() {
             for (y_pos, tile) in row.iter().enumerate() {
@@ -135,12 +153,9 @@ async fn main() {
                 }
             }
         }
+        let tile_image_time = &tile_image_time.elapsed().as_secs_f32().to_string();
 
-        // Draw tile image
-        texture.update(&image);
-        draw_texture(texture, 0.0, 0.0, WHITE);
-
-        // Draw texture
+        // Draw Cursor
         draw_rectangle(
             mouse_x - TILE_SIZE / 2.0,
             mouse_y - TILE_SIZE / 2.0,
@@ -149,9 +164,35 @@ async fn main() {
             Color::new(1.0, 0.0, 0.0, 1.0),
         );
 
-        // Draw fps text
-        let fps: &str = &get_fps().to_string();
+        let draw_texture_time = Instant::now();
+        // Draw tile image
+        texture.update(&image);
+        draw_texture(texture, 0.0, 0.0, WHITE);
+        let draw_texture_time = &draw_texture_time.elapsed().as_secs_f32().to_string();
+
+        // Draw fps and rendering time text
         draw_text(fps, 10.0, 15.0, 25.0, WHITE);
+        draw_text(
+            &("Update sim: ".to_owned() + update_sim_time),
+            10.0,
+            35.0,
+            25.0,
+            WHITE,
+        );
+        draw_text(
+            &("Tile image creation: ".to_owned() + tile_image_time),
+            10.0,
+            55.0,
+            25.0,
+            WHITE,
+        );
+        draw_text(
+            &("Draw texture: ".to_owned() + draw_texture_time),
+            10.0,
+            75.0,
+            25.0,
+            WHITE,
+        );
 
         next_frame().await
     }
